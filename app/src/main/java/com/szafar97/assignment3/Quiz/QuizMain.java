f966c5cf9dde581152fe42b4f59ad091ca9a7ecb
Shoaib Zafar <szafar97@gmail.com>
2020-12-20 02:26:29 +0500

    Empty Activities created for Quiz

---
+++ app/src/main/java/com/szafar97/assignment3/Quiz/QuizMain.java
@@ -0,0 +1,16 @@
+package com.szafar97.assignment3.Quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.szafar97.assignment3.R;
+
+public class QuizMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_main);
+    }
+}

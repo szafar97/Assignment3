6a939e0cefb28d5c7b577b76934f309b9c9becbc
Shoaib Zafar <szafar97@gmail.com>
2020-12-20 20:30:00 +0500

    Fixed Launcher Intent

--- app/src/main/java/com/szafar97/assignment3/SplashActivity.java
+++ app/src/main/java/com/szafar97/assignment3/SplashActivity.java
@@ -23,6 +23,7 @@ public class SplashActivity extends AppCompatActivity {
                 finally{
                     Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                     startActivity(intent);
+                    finish();
                 }
             }
         };
